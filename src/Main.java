79765b8121c5ace43878260f7b3775825f63e91b
skyluckbai <kevin.bai@webcomm.com.tw>
2023-08-27 16:08:23 +0800

    update RBTree

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        RBTree<String> bst = new RBTree<String>();
+        bst.addNode("d");
+        bst.addNode("d");
+        bst.addNode("c");
+        bst.addNode("c");
+        bst.addNode("b");
+        bst.addNode("f");
+
+        bst.addNode("a");
+        bst.addNode("e");
+
+        bst.addNode("g");
+        bst.addNode("h");
+
+
+        bst.remove("c");
+
+        bst.printTree();
     }
 }
